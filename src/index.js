59efe6706496b367cfa07f1c1032b34ffca35699
gal <tanjhu@163.com>
2020-07-01 10:41:09 +0800

    开始代码+填入数字

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './App';
+import * as serviceWorker from './serviceWorker';
+
+
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
